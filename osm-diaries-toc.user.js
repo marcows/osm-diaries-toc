fa2a134816676390db05e6273e785be56c2d179a
Markus Heidelberg <markus.heidelberg@web.de>
2016-02-18 10:17:31 +0100

    Add a table of contents at the top of the OSM diary page
    For an overview of articles and corresponding comments.
    
    It contains two columns:
      1) link to the article
      2) link to the article comments

---
+++ osm-diaries-toc.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name        OSM diaries TOC
+// @namespace   https://github.com/marcows
+// @description Adds a table of contents to the OpenStreetMap diaries page and highlights new posts and comments.
+// @include     *://www.openstreetmap.org/diary*
+// @version     0.1.0
+// @icon        http://www.openstreetmap.org/assets/osm_logo.png
+// @grant       none
+// ==/UserScript==
+
+var articleLinks = document.querySelectorAll(".diary_post h2 > a");
+var commentLinks = document.querySelectorAll(".diary_post .secondary-actions a[href$=comments]");
+
+var toc = document.createElement("table");
+
+/* Create table of contents. */
+for (var i = 0; i < articleLinks.length && i < commentLinks.length; i++) {
+	var tablerow = document.createElement("tr");
+	var cell;
+
+	/* Column 1: link to article in own page */
+	cell = document.createElement("td");
+	cell.appendChild(articleLinks[i].cloneNode(true));
+	tablerow.appendChild(cell);
+
+	/* Column 2: link to article comments in own page */
+	cell = document.createElement("td");
+	cell.appendChild(commentLinks[i].cloneNode(true));
+	tablerow.appendChild(cell);
+
+	toc.appendChild(tablerow);
+}
+
+document.querySelector(".content-body .content-inner").insertBefore(toc, document.querySelector(".diary_post"));
