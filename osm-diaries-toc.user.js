a0dd4f0df733db518d92d15fa5671c1331e2768c
Markus Heidelberg <markus.heidelberg@web.de>
2016-02-21 17:29:46 +0100

    Insert a first column with arrow down links for scrolling to articles

--- osm-diaries-toc.user.js
+++ osm-diaries-toc.user.js
@@ -16,17 +16,31 @@ var toc = document.createElement("table");
 /* Create table of contents. */
 for (var i = 0; i < articleLinks.length && i < commentLinks.length; i++) {
 	var tablerow = document.createElement("tr");
-	var cell;
+	var cell, link, anchor;
 
-	/* Column 1: link to article in own page */
+	/* Column 1: anchor link for scrolling down to article */
+	anchor = "article" + i;
+	link = document.createElement("a");
+	link.innerHTML = "&dArr;";
+	link.href = "#" + anchor;
+
+	cell = document.createElement("td");
+	cell.appendChild(link);
+	tablerow.appendChild(cell);
+
+	/* Column 2: link to article in own page */
 	cell = document.createElement("td");
 	cell.appendChild(articleLinks[i].cloneNode(true));
 	tablerow.appendChild(cell);
 
-	/* Column 2: link to article comments in own page */
+	/* Column 3: link to article comments in own page */
 	cell = document.createElement("td");
 	cell.appendChild(commentLinks[i].cloneNode(true));
 	tablerow.appendChild(cell);
+
+	// Anchor name of the article if not existing yet
+	if (!articleLinks[i].name)
+		articleLinks[i].name = anchor;
 
 	toc.appendChild(tablerow);
 }
